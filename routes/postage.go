79289f0bc872b55f31920fe6f5abba7422a1e32c
Ahmad Ayubi <aayub041@uottawa.ca>
2020-12-29 22:08:59 -0500

    Removes unused endpoints

--- routes/postage.go
+++ routes/postage.go
@@ -11,8 +11,6 @@ func PostageRoutes() *chi.Mux{
 	router:= chi.NewRouter()
 
 	router.With(middleware.ProtectedApprovedUserRoute).Post("/buy/canadapost", controllers.BuyCanadaPostPostageLabel)
-	router.With(middleware.ProtectedApprovedUserRoute).Get("/employee/approved/{employeeID}",controllers.IsEmployeeApproved)
-	router.With(middleware.ProtectedApprovedUserRoute).Get("/details", controllers.GetCompanyDetails)
 
 	return router
 }
